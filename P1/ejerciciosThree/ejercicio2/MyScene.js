b24a4154e75eccf0dafbfdbf246988268696e9c4
Carmenchu <carmenxfdz@gmail.com>
2024-03-02 19:28:05 +0100

    Update MyScene.js

---
+++ P1/ejerciciosThree/ejercicio2/MyScene.js
@@ -0,0 +1,10 @@
+// Clases de la biblioteca
+
+import * as THREE from '../../libs/three.module.js'
+import { GUI } from '../../libs/dat.gui.module.js'
+import { TrackballControls } from '../../libs/TrackballControls.js'
+import { Stats } from '../../libs/stats.module.js'
+
+// Clases del Proyect
+
+import { Cube } from './Cube.js'
